a4c19bc4fa3b4daddde4872b1c27939ad74f17b4
duc.nh <duc.nh@gmail.com>
2016-07-13 22:32:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/animation/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.user.animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonTranslate;
+    private Button mButtonAlpha;
+    private Button mButtonScale;
+    private Button mButtonRotate;
+    private Animation mAnimation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Animation animTranslate = AnimationUtils.loadAnimation(this,R.anim.anim_translate);
+        final Animation animAlpha = AnimationUtils.loadAnimation(this,R.anim.anim_alpha);
+        final Animation animScale = AnimationUtils.loadAnimation(this,R.anim.anim_scale);
+        final Animation animRotate = AnimationUtils.loadAnimation(this,R.anim.anim_rotate);
+        mButtonTranslate = (Button) findViewById(R.id.translate);
+        mButtonAlpha = (Button) findViewById(R.id.alpha);
+        mButtonScale = (Button) findViewById(R.id.scale);
+        mButtonRotate = (Button) findViewById(R.id.rotate);
+
+        mButtonTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animTranslate);
+            }
+        });
+
+        mButtonAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animAlpha);
+            }
+        });
+
+        mButtonScale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animScale);
+            }
+        });
+
+        mButtonRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(animRotate);
+            }
+        });
+
+
+    }
+}
